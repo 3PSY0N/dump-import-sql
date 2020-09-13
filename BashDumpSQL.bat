95541893798f619e5d44c2cb4675e87e0aa290c5
3PSY0N <3511559-3PSY0N@users.noreply.gitlab.com>
2020-09-13 17:29:22 +0000

    Update BashDumpSQL.bat

--- BashDumpSQL.bat
+++ BashDumpSQL.bat
@@ -2,6 +2,7 @@
 
 set TODAY_DATE=%date:~6,4%%date:~3,2%%date:~0,2%
 set TABLE_LIST=(table1 table2 table3 table4 table5)
+set DB_USER=root
 set DEST_DIR=C:\fakeroot\
 
 echo ----------
@@ -10,7 +11,7 @@ echo ----------
 
 for %%t in %TABLE_LIST% do (
 	echo Dump table: %%t
-	mysqldump -u root -p %%t > %DEST_DIR%%%t_%TODAY_DATE%.sql
+	mysqldump -u %DB_USER% -p %%t > %DEST_DIR%%%t_%TODAY_DATE%.sql
 	echo.
 )
 
