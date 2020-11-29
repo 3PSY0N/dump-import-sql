9b375d2ff2352c26cc0d1a88662cde1a105051ed
3PSY0N <3511559-3PSY0N@users.noreply.gitlab.com>
2020-11-29 23:30:49 +0100

    Update Dump batch

--- BashDumpSQL.bat
+++ BashDumpSQL.bat
@@ -1,25 +1,29 @@
 @echo off
 
-set DB_USER=root
 set TODAY_DATE=%date:~6,4%%date:~3,2%%date:~0,2%
+set REMOVE_LIST=(performance_schema information_schema)
 set DEST_DIR=C:\fakeroot\
-set TABLE_LIST=(Database mysql performance_schema information_schema)
+set DB_USER=root
 
 echo ----------
 echo Starting SQL Dump
 echo ----------
 
+:: Add -p and your password after %DB_USER% if needed.
 FOR /F "tokens=*" %%D IN ('mysql -u %DB_USER% -e "SHOW DATABASES"') DO (
 	echo Dump table: %%D
 	mysqldump -u %DB_USER% %%D > %DEST_DIR%%TODAY_DATE%_%%D.sql
+	echo.
 )
 
-
-FOR %%t IN %TABLE_LIST% DO (
+FOR %%t IN %REMOVE_LIST% DO (
  	echo Del file: %%t
 	del "%DEST_DIR%%TODAY_DATE%_%%t.sql" /s /f /q
+	echo.
 )
 
 echo ----------
 echo Dump completed
 echo ----------
+
+pause;
